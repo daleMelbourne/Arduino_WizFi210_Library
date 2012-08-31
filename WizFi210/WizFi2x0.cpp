bda7d50ff4f7abba25f1e61b88c86d22ffc8b2f8
James YS Kim <javakys@gmail.com>
2012-08-31 18:03:06 +0900

    WiFi2x0Class was Updated
    1. ReplyCheckTimer and corresponding funtions were added
    2. CheckReply() was updated
    3. ParseReply() was updated
    4. GetToken() was added

--- WizFi210/WizFi2x0.cpp
+++ WizFi210/WizFi2x0.cpp
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <string.h>
 #include "WizFi2x0.h"
 
 ////////////////////////////////
@@ -23,9 +25,12 @@ void WizFi2x0Class::init(void)
 	delay(500);
 
 	digitalWrite(WizFi2x0_RST, HIGH);
-	delay(6000);
+	delay(3000);
 
-	
+	ReplyCheckTimer.init();
+
+	Current_CmdState = WizFi2x0_CmdState_IDLE;
+
 }
 
 boolean WizFi2x0Class::Setup(void)
@@ -52,7 +57,17 @@ uint8_t WizFi2x0Class::write(byte ch)
 
 uint8_t WizFi2x0Class::write(byte *buf, size_t length)
 {
-	return (uint8_t)length;
+	uint16_t i;
+
+	for(i=0; i<(uint16_t)length; i++)
+	{
+		write(buf[i]);
+		//delay(10);
+	}
+
+	memset((char *)MsgBuf, 0, sizeof(MsgBuf));
+	
+	return (uint8_t)i;
 }
 
 uint8_t WizFi2x0Class::read(void)
@@ -70,6 +85,271 @@ uint8_t WizFi2x0Class::read(byte *buf)
 {
 }
 
+void WizFi2x0Class::SendCommand()
+{
+	write(MsgBuf, SendByte);
+	RxIdx = 0;
+	ReplyCheckTimer.TimerStart(6000);
+	Current_CmdState = WizFi2x0_CmdState_Sent;
+	Current_Command_Code = OP_AT;
+}
+
+uint8_t WizFi2x0Class::CheckReply(void)
+{
+	byte tmp;
+
+	tmp = read();
+	if((tmp != spichar.SPI_IDLE_CHAR) && (tmp != spichar.SPI_INVALID_CHAR_ALL_ZERO) && (tmp != spichar.SPI_INVALID_CHAR_ALL_ONE))
+	{
+		MsgBuf[RxIdx++] = tmp;
+	}
+
+	if(tmp == ']')
+	{
+		/////////////////////////////////////////	
+		//Flush rx data
+		while(digitalRead(WizFi2x0_DataReady) == HIGH)
+		{
+			tmp = read();
+		}
+		//
+		////////////////////////////////////////
+		
+		if(ParseReply(MsgBuf))
+			return 1; // Reply is OK
+		else
+			return 2; // Reply is FAILED
+	}
+
+	return 0; // Reply is in progress
+	
+}
+
+uint8_t WizFi2x0Class::ParseReply(byte * buf)
+{
+	int retval;
+	uint8_t Token[33];
+	uint8_t tmpToken[33];
+	uint8_t APListIndex;
+	
+	switch(Current_Command_Code)
+	{
+	case OP_AT: 
+	case OP_ATE:
+	case OP_WD:
+	case OP_WWEP:
+	case OP_WWPA:
+	case OP_NDHCP:
+	case OP_WAUTO:
+	case OP_NAUTO:
+	case OP_NSET:
+	case OP_ATO:
+		Current_Ptr = 0;
+		retval = GetToken(Token);
+
+		while(1)
+		{
+			if(!strcmp((char const *)Token, "[OK]"))
+				return 1;
+			else
+			{
+				retval = GetToken(Token);
+//				printf("\r\n Token Length: %d, Token : %s", retval, Token);
+				if(!strcmp((char const *)Token, "[OK]"))
+					return 1;
+				if(retval == -1)
+					return 0;
+
+			}
+		}
+	case OP_WS:
+
+		Current_Ptr = 0;
+		retval = GetToken(Token);
+
+		return 1;
+#if 0
+		while(1)
+		{
+			if(!strcmp((char const *)Token, "BSSIDSSIDChannelTypeRSSISecurity"))
+			{
+				retval = GetToken(Token);
+//				printf("\r\n Token Length: %d, Token : %s", retval, Token);
+				memset(tmpToken, 0, 33);
+				memcpy(tmpToken, Token, 13);
+				APListIndex = WIZFI210_GetAPListCount();
+				while(strcmp((char const *)tmpToken, "No.OfAPFound:")!=0)
+				{
+//					printf("\r\nBSSI: %s", Token);
+					WIZFI210_SetBSSID(APListIndex, Token);
+					
+					retval = GetToken(Token);
+//					printf("\tSSID: %s", Token);
+					WIZFI210_SetSSID(APListIndex, Token);
+					
+					retval = GetToken(Token);
+//					printf("\t\tChannel: %s", Token);
+					WIZFI210_SetChannel(APListIndex, Token);
+					
+					retval = GetToken(Token);
+//					printf("\tType: %s", Token);
+					WIZFI210_SetType(APListIndex, Token);
+
+					retval = GetToken(Token);
+//					printf("\tRSSI: %s", Token);
+					WIZFI210_SetRSSI(APListIndex, Token);
+
+					retval = GetToken(Token);
+//					printf("\t\tSecurity: %s", Token);
+					WIZFI210_SetSecurity(APListIndex, Token);
+
+					APListIndex++;
+					WIZFI210_SetAPListCount(APListIndex);
+					
+					retval = GetToken(Token);
+					memset(tmpToken, 0, 33);
+					memcpy(tmpToken, Token, 13);
+				}
+				retval = GetToken(Token);
+//				printf("\r\n %s", Token);
+				if(!strcmp((char const *)Token, "[OK]"))
+					return 1;
+				else
+					return 0;
+			}else
+			{
+				retval = GetToken(Token);
+				if(retval == -1)
+					return 0;
+			}
+		}
+#endif
+	case OP_ATA:
+		Current_Ptr = 0;
+		retval = GetToken(Token);
+//		printf("\r\n Token Length: %d, Token : %s", retval, Token);
+
+		while(1)
+		{
+			if(!strcmp((char const *)Token, "[OK]"))
+			{
+				return 1;
+			}
+			else
+			{
+				memset(tmpToken, 0, 33);
+				memcpy(tmpToken, Token, 7);
+				if(!strcmp((char const *)tmpToken, "[ERROR]"))
+					return 0;
+				retval = GetToken(Token);
+//				printf("\r\n Token Length: %d, Token : %s", retval, Token);
+				if(!strcmp((char const *)Token, "[OK]"))
+				{
+					return 1;
+				}
+				if(retval == -1)
+					return 0;
+			}
+		}
+	case OP_ATCID:
+		Current_Ptr = 0;
+		retval = GetToken(Token);
+
+		while(1)
+		{
+			if(!strcmp((char const *)Token, "NovalidCids"))
+			{				
+				return 1;
+			}
+			else if(!strcmp((char const *)Token, "CIDTYPEMODELOCALPORTREMOTEPORTREMOTEIP"))
+			{
+				return 1;
+			}
+			retval = GetToken(Token);
+//			printf("\r\n Token Length: %d, Token : %s", retval, Token);			
+			if(retval == -1)
+				return 0;
+		}
+	}
+	return 0;
+}
+
+int WizFi2x0Class::GetToken(uint8_t * Token)
+{
+	uint8_t i = 0;
+	memset(Token, 0, 33);
+
+	while(Current_Ptr < RxIdx)
+	{
+		if(MsgBuf[Current_Ptr] != ',' && MsgBuf[Current_Ptr] != 0x0D && MsgBuf[Current_Ptr] != 0x0A && MsgBuf[Current_Ptr] != '\0')
+		{
+			if(MsgBuf[Current_Ptr] != ' ' && MsgBuf[Current_Ptr]!= '\t')
+				Token[i++] = MsgBuf[Current_Ptr++];
+			else
+				Current_Ptr++;
+		}
+		else
+		{
+			if(MsgBuf[Current_Ptr] == ',')
+			{
+				Current_Ptr++;
+				return i;
+			}else if(MsgBuf[Current_Ptr] == 0x0D)
+			{
+				if(MsgBuf[Current_Ptr + 1] == 0x0A)
+				{
+					Current_Ptr += 2;
+					return i;
+				}else
+				{
+					Current_Ptr++;
+					return i;
+				}
+			}else if(MsgBuf[Current_Ptr] == 0x0A)
+			{
+				if(MsgBuf[Current_Ptr + 1] == 0x0D)
+				{
+					Current_Ptr += 2;
+					return i;
+				}else
+				{
+					Current_Ptr++;
+					return i;
+				}
+			}else if(MsgBuf[Current_Ptr] == '\0')
+			{
+				Current_Ptr++;
+				return i;
+			}
+		}
+	}
+
+	return -1;
+}
+
+uint8_t WizFi2x0Class::CheckSyncReply(void)
+{
+	byte key;
+
+	key = read();
+	if((key != spichar.SPI_IDLE_CHAR) && ( key != spichar.SPI_INVALID_CHAR_ALL_ZERO) && (key != spichar.SPI_INVALID_CHAR_ALL_ONE))
+	{
+		;
+	}
+
+	if(digitalRead(WizFi2x0_DataReady) == LOW)
+		return 1;
+
+	delay(10); // wait during 10ms
+	
+	return 0;
+}
+
+void WizFi2x0Class::SendSync(void)
+{
+	write(spichar.SPI_IDLE_CHAR);	
+}
+
 boolean WizFi2x0Class::ByteStuff(byte *ch)
 {
 	if((*ch == spichar.SPI_ESC_CHAR) ||
@@ -85,3 +365,50 @@ boolean WizFi2x0Class::ByteStuff(byte *ch)
 	}else
 		return false;
 }
+
+
+void TimeoutClass::init(void)
+{
+	TimerValue = 0;
+	IsTimeout = false;
+	TimerCount = 0;
+	IsTimerStarted = false;
+}
+
+
+void TimeoutClass::TimerStart(uint16_t timevalue)
+{
+	TimerValue = timevalue;
+	TimerStart();
+}
+
+void TimeoutClass::TimerStart(void)
+{
+	IsTimeout = false;
+	TimerCount = 0;
+	IsTimerStarted = true;
+}
+
+void TimeoutClass::TimerStop(void)
+{
+	IsTimerStarted = false;
+}
+
+boolean TimeoutClass::GetIsTimeout(void)
+{
+	return IsTimeout;
+}
+
+void TimeoutClass::CheckIsTimeout(void)
+{
+	if(IsTimerStarted)
+	{
+		if(TimerCount++ >= TimerValue)
+		{
+			IsTimeout = true;
+			TimerStop();
+		}
+	}
+}
+
+
--- WizFi210/WizFi2x0.h
+++ WizFi210/WizFi2x0.h
@@ -7,6 +7,30 @@
 #define WizFi2x0_DataReady	3
 #define WizFi2x0_CS			4
 
+#define WizFi2x0_CmdState_IDLE	0x01
+#define WizFi2x0_CmdState_Sent	0x02
+#define WizFi2x0_CmdState_Rcvd	0x03
+
+enum CMDOP {
+	OP_AT = 1, 
+	OP_ATE = 2, 
+	OP_WS = 3, 
+	OP_WD = 4, 
+	OP_WAUTO = 5, 
+	OP_NAUTO = 6, 
+	OP_NDHCP = 7, 
+	OP_WWEP = 8, 
+	OP_WWPA = 9, 
+	OP_ATA = 10, 
+	OP_NSET = 11, 
+	OP_ATCID = 12, 
+	OP_ATO = 13, 
+	OP_NCTCP = 14, 
+	OP_NCLOSE = 15, 
+	OP_NCLOSEALL = 16, 
+	OP_CMDMODE = 17
+};
+
 class SPIChar
 {
 public:
@@ -19,11 +43,36 @@ public:
 	static const byte SPI_LINK_READY = 0xF3;
 };
 
+class TimeoutClass
+{
+private:
+	boolean IsTimerStarted;
+	uint16_t TimerCount;
+	uint16_t TimerValue;
+	boolean IsTimeout;
+public:
+	void init(void);
+	virtual void TimerStart(uint16_t timevalue);
+	virtual void TimerStart(void);
+	void TimerStop(void);
+	boolean GetIsTimeout(void);	
+	void CheckIsTimeout();
+};
+
 class WizFi2x0Class
 {
 public:
 	byte MsgBuf[128];
+	uint16_t RxIdx;
+	uint8_t SendByte;
+	
 	SPIChar spichar;
+	TimeoutClass ReplyCheckTimer;
+	uint8_t Current_CmdState;
+	uint8_t Current_Command_Code;
+	uint16_t Current_Ptr;
+private:
+	
 public:
 	void init(void);
 	boolean Setup(void);
@@ -35,6 +84,13 @@ public:
 	virtual uint8_t read();
 	virtual uint8_t read(byte *buf);
 	boolean ByteStuff(byte *str);
+	uint8_t CheckReply(void);
+	uint8_t CheckSyncReply(void);
+	uint8_t ParseReply(byte *buf);
+	void SendSync(void);
+	int GetToken(uint8_t * Token);
+	void SendCommand(void);
+
 };
 
 #endif
